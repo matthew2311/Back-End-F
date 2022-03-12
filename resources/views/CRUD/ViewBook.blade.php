ec9b7caefac8b5112f247760af01bd3e0a97ea42
Matthew <matthewsebastian23@gmail.com>
2022-03-13 01:51:43 +0700

    Pertemuan-10

--- resources/views/CRUD/ViewBook.blade.php
+++ resources/views/CRUD/ViewBook.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+<marquee style="max-width: 540px; margin-left: 32.5%"><a href="{{Route('ViewAll')}}">Click Me to Return</a></marquee>
     <div class="row justify-content-center">
         <div class="card mb-3" style="max-width: 540px; border-radius:15px" >
             <div class="row g-0">
@@ -9,10 +10,18 @@
               </div>
               <div class="col-md-8">
                 <div class="card-body" style="margin-left: 10px">
-                  <h3 class="card-title">{{$book->nama}} - ({{$book->penulis}})</h3>
-                  <p class="card-text">Harga: Rp.{{($book->harga)}},00</p>
-                  <p class="card-text">Stock: {{($book->stock)}} Qty</p>
-                  <a href="{{Route('ViewAll')}}">Click Me to Return</a>
+                    <h3 class="card-title">{{$book->nama}} - ({{$book->penulis}})</h3>
+                    <p class="card-text">Harga: Rp.{{($book->harga)}},00</p>
+                    <p class="card-text">Stock: {{($book->stock)}} Qty</p>
+                    <p class="card-text">Category:
+                        @foreach ($book->category as $category)
+                            @if ($loop->last)
+                                {{$category->category_name}}
+                            @else
+                                {{$category->category_name}},
+                            @endif
+                        @endforeach
+                    </p>
                 </div>
               </div>
             </div>
